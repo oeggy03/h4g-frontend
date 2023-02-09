141a4c6abfaecf5db3d98894d2917dcbb4bf4daa
tran ha thu <hathu2516@gmail.com>
2023-02-09 23:15:42 +0800

    Sign in, sign up, sign out works with jwt token support 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/Auth/Auth.css 	new file:   src/components/Auth/Signin.js 	new file:   src/components/Auth/Signup.js 	new file:   src/components/Homepage/Homepage.js 	new file:   src/components/Navbar/Navbar.css 	new file:   src/components/Navbar/Navbar.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import 'tachyons';
+import Signup from './components/Auth/Signup';
+import Homepage from './components/Homepage/Homepage';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  const [isSignedIn, setSignedIn] = useState(false); //tracks whether user is signed in
+
+  //user details, which we can send to the Profile component instead of getUser again.
+  const [username, setUsername] = useState("")
+  const [name, setName] = useState("")
+  const [phone, setPhone] = useState("")
+  const [email, setEmail] = useState("")
+  const [userid, setID] = useState("")
+  const [type, setType] = useState("")
+
+
+  //We retrieve the user's data first. 
+  //Data can only be retrieved if the user has signed in before due to the jwt token
+  //jwt prevents them from signing out if they close the page
+  useEffect(() => {
+    const fetchOptions = {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'include' //Required for JWT tokens to work
+    }
+
+    fetch('http://localhost:3001/api/GetUser', fetchOptions)
+    .then(response => {
+        if (response.status === 200) {setSignedIn(true)} 
+
+        return response.json()})
+    .then(res => {
+        setUsername(res.username)
+        setID(res.id)
+        setEmail(res.email)
+        setName(res.name)
+        setPhone(res.phone)
+        setType(res.type)
+    });
+  })
+
+  //A function that can be passed as a prop, to update whether the user is signed in
+  //status is boolean
+  function UpdateSignedIn (status) {
+    setSignedIn(status)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <Navbar updateSI = {UpdateSignedIn} statusSI = {isSignedIn}/>
+    <div className='appContainer'>
+      <Routes>
+        <Route path = "/" element = {<Homepage updateSI = {UpdateSignedIn} statusSI = {isSignedIn}/>} />
+        <Route path = "/signup" element = {<Signup updateSI = {UpdateSignedIn}/>} />
+      </Routes>
     </div>
-  );
+  </div>);
 }
 
 export default App;
